b3f6f871e37680ac676a5237e9d8ac25be794113
vidovb <vidovb@taltech.ee>
2025-03-26 23:48:16 +0200

    Ticket-101 fix: updated maximum loan period constraint

--- src/main/java/ee/taltech/inbankbackend/config/DecisionEngineConstants.java
+++ src/main/java/ee/taltech/inbankbackend/config/DecisionEngineConstants.java
@@ -6,12 +6,13 @@ package ee.taltech.inbankbackend.config;
 public class DecisionEngineConstants {
     public static final Integer MINIMUM_LOAN_AMOUNT = 2000;
     public static final Integer MAXIMUM_LOAN_AMOUNT = 10000;
-    public static final Integer MAXIMUM_LOAN_PERIOD = 60;
+    public static final Integer MAXIMUM_LOAN_PERIOD = 48; // fixed
     public static final Integer MINIMUM_LOAN_PERIOD = 12;
     public static final Integer SEGMENT_1_CREDIT_MODIFIER = 100;
     public static final Integer SEGMENT_2_CREDIT_MODIFIER = 300;
     public static final Integer SEGMENT_3_CREDIT_MODIFIER = 1000;
 
+    // TICKET-102
     public static final Float LIFE_EXPECTANCY_ESTONIA = 78.1f;
     public static final Float LIFE_EXPECTANCY_LATVIA = 74.5f;
     public static final Float LIFE_EXPECTANCY_LITHUANIA = 75.6f;
